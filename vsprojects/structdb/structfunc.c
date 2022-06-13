efc137122ef4ab0d0696d51922e45b9630c61d5c
Dave <bigmanrunning@gmail.com>
2022-06-13 13:52:28 -0500

    structfunc 06/13/2022

--- vsprojects/structdb/structfunc.c
+++ vsprojects/structdb/structfunc.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+
+//Got the answer to putting a function in a struct from this guy:
+//  https://stackoverflow.com/a/32964582/13972774
+//Brilliant idea.
+
+
 //Build the struct, the member is:
 // pointer to someFunction, typecast to an int
 //The typedef creates hello as the variable "type"
@@ -7,10 +13,22 @@ typedef struct hello {
     int (*someFunction)();
 } hello;
 
+typedef struct sum {
+    int a;
+    int b;
+    int (*add2nums)(int a, int b);
+} sum;
+
 //Declare a function that returns the int for *someFunction
 int foo() {
     return 0;
 }
+
+int add(int a, int b){
+
+     return a + b;
+}
+
 
 //Declare a function that returns the struct type of
 //our typedef'ed struct called hello
@@ -26,15 +44,31 @@ hello Hello() {
     return aHello;
 }
 
+sum Sum(){
+     struct sum mySum;
+     mySum.add2nums = &add;
+
+     return mySum;
+}
+
+
+
 int main()
 {
     //create a struct and init it to Hello function
     //is Hello() now a constructor?
     struct hello aHello = Hello();
+    struct sum aSum = Sum();
 
     //Now, when we call that someFunction member,
     //it runs the foo function and returns a 0 to printf
     printf("Print hello: %d\n", aHello.someFunction());
+
+    aSum.a = 3;
+    aSum.b = 3;
+
+    printf("The sum is: %d\n", aSum.add2nums(aSum.a, aSum.b));
+
 
     return 0;
 } 
