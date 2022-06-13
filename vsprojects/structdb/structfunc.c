bc101c172c53d11f78178ceec98fbf65544dcf0d
Dave <bigmanrunning@gmail.com>
2022-06-13 13:23:01 -0500

    structfunc 06/13/2022

---
+++ vsprojects/structdb/structfunc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//Build the struct, the member is:
+// pointer to someFunction, typecast to an int
+//The typedef creates hello as the variable "type"
+typedef struct hello {
+    int (*someFunction)();
+} hello;
+
+//Declare a function that returns the int for *someFunction
+int foo() {
+    return 0;
+}
+
+//Declare a function that returns the struct type of
+//our typedef'ed struct called hello
+hello Hello() {
+    //declare a new hello struct called aHello
+    struct hello aHello;
+
+    //Tell aHello member "pointer to someFunction"
+    //to point to function foo
+    aHello.someFunction = &foo;
+
+    //now return the init-ed struct
+    return aHello;
+}
+
+int main()
+{
+    //create a struct and init it to Hello function
+    //is Hello() now a constructor?
+    struct hello aHello = Hello();
+
+    //Now, when we call that someFunction member,
+    //it runs the foo function and returns a 0 to printf
+    printf("Print hello: %d\n", aHello.someFunction());
+
+    return 0;
+} 
