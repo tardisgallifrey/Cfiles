0329e36437fca142c4d4be288a980358ea0070ff
tardisagllifrey <bigmanrunning@gmail.com>
2022-06-22 18:31:36 -0500

    OpenGL beginnings -6/22/2022

---
+++ OpenGl/hello/build/CMakeFiles/OpenGLHello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLHello.dir/openglhello.c.o"
+  "CMakeFiles/OpenGLHello.dir/openglhello.c.o.d"
+  "OpenGLHello"
+  "OpenGLHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OpenGLHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
